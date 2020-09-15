1bb61d9033b6d48e5470b547b3864b755820e24c
farzam <farzam.muhammad@yahoo.com>
2020-09-14 17:34:00 -0700

    integerate firebase databae with order

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
 import Todo from "./Todo";
+import { fdb } from "./firebase";
+import firebase from "firebase";
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState(["apple", "grapes"]);
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
+
+  useEffect(() => {
+    fdb
+      .collection("todos")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        //console.log(snapshot.docs.map((doc) => doc.data()));
+        setTodos(snapshot.docs.map((doc) => doc.data().todo));
+      });
+  }, []);
 
   const addTodos = (e) => {
     e.preventDefault();
-    setTodos([...todos, input]);
+    fdb.collection("todos").add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
     setInput("");
   };
   return (
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,16 @@
 import firebase from "firebase";
 
-const firebaseApp = firebase.initializeApp({});
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBrAhmHf62_aQrCQG7aqLSf2xUYTjSdne8",
+  authDomain: "todo-app-proj.firebaseapp.com",
+  databaseURL: "https://todo-app-proj.firebaseio.com",
+  projectId: "todo-app-proj",
+  storageBucket: "todo-app-proj.appspot.com",
+  messagingSenderId: "706348180495",
+  appId: "1:706348180495:web:0eea9765cb8e12da602f7a",
+  measurementId: "G-DJDMZLZEYC",
+});
+
+const db = firebaseApp.firestore();
+
+export { db as fdb };
